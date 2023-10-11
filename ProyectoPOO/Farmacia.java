04958c59438e2ca0edf3766e803723685077cd5b
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-10-10 23:55:18 -0600

    update Farmacia

--- ProyectoPOO/Farmacia.java
+++ ProyectoPOO/Farmacia.java
@@ -1,5 +1,19 @@
 package ProyectoPOO;
+import java.util.ArrayList; 
+import java.util.List;
+import java.util.concurrent.LinkedTransferQueue; 
+public class Farmacia { 
+    private String NombreFarmacia; 
+    private List<String> ListadoFarmacias; 
 
-public class Farmacia {
-       
+    //constructor...    
+    public Farmacia (String NombreFarmacia) {  
+        this.NombreFarmacia = NombreFarmacia; 
+        this.ListadoFarmacias = new ArrayList<>(); 
+    }
+
+    //metodos para agregar una farmacia... 
+    public void agregarFarmacia (String NombreFarmacia) { 
+        ListadoFarmacias.add(NombreFarmacia); 
+    }
 }
