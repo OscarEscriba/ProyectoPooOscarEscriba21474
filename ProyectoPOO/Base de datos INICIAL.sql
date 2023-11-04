609860b8300718332252006cb4070fcaeaa4724f
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2023-11-04 01:16:32 -0600

    Update Base de datos INICIAL.sql

--- ProyectoPOO/Base de datos INICIAL.sql
+++ ProyectoPOO/Base de datos INICIAL.sql
@@ -1,19 +1,19 @@
-create database proyecto; 
-use proyecto; 
+create database proyectoss; 
+use proyectoss; 
 create table medico (  
-id int auto_increment, 
+id int auto_increment primary key, 
 nombre varchar(255) not null, 
 especialidad varchar(255) not null, 
 ubicacion varchar(255) not null 
 );  
 create table pacientes(  
-id int auto_increment, 
+id int auto_increment primary key, 
 nombre varchar(255) not null, 
 sintomas TEXT, 
 medicamentos_consumidos TEXT
 );  
 
 create table farmacia ( 
-id int auto_increment, 
+id int auto_increment primary key, 
 ubicaciones TEXT
 ); 
